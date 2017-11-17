4c998e1e4ed16526738b5bdadbe442e4254958dd
xlwei920@163.com <xlwei920@163.com>
2017-11-17 15:01:10 +0800

    eight imgs

---
+++ app/src/main/java/com/huayinghealth/protecteyes/fragment/EightRadioFragment.java
@@ -0,0 +1,7 @@
+package com.huayinghealth.protecteyes.fragment;
+
+/**
+ * Created by ChanLin on 2017/11/17.
+ */
+public class EightRadioFragment {
+}
--- app/src/main/java/com/huayinghealth/protecteyes/fragment/FragmentEight.java
+++ app/src/main/java/com/huayinghealth/protecteyes/fragment/FragmentEight.java
@@ -4,17 +4,24 @@ package com.huayinghealth.protecteyes.fragment;
 import android.app.Fragment;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.huayinghealth.protecteyes.R;
 
+import java.util.ArrayList;
+
 /**
  * Created by ChanLin on 2017/11/15.
  */
 public class FragmentEight extends Fragment {
 
+    private ViewPager viewPager;
+    private FragmentPagerAdapter adapter;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -24,5 +31,11 @@ public class FragmentEight extends Fragment {
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
+        setView();
+    }
+    private void setView(){
+        viewPager = (ViewPager)getActivity().findViewById(R.id.vp_eight_imgs);
+        ArrayList list = new ArrayList();
+        list.add();
     }
 }
