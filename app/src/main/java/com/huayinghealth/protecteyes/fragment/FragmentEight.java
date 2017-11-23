e5458f2939a25c80a6976ae618f4f395a3fa7ba7
yangcl1056 <1056552156@qq.com>
2017-11-23 19:15:55 +0800

    fragment_eight

--- app/src/main/java/com/huayinghealth/protecteyes/fragment/FragmentEight.java
+++ app/src/main/java/com/huayinghealth/protecteyes/fragment/FragmentEight.java
@@ -11,6 +11,7 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.huayinghealth.protecteyes.R;
 
@@ -22,12 +23,14 @@ import java.util.ArrayList;
 public class FragmentEight extends Fragment {
 
     private ViewPager viewPager;
-    private View img1,img2;
+    private View img1, img2, img3, img4;
     private FragmentPagerAdapter adapter;
+    private TextView tx_eight_title, tv_operate;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_eight,container,false);
+        return inflater.inflate(R.layout.fragment_eight, container, false);
     }
 
     @Override
@@ -35,19 +38,27 @@ public class FragmentEight extends Fragment {
         super.onActivityCreated(savedInstanceState);
         setView();
     }
-    private void setView(){
-        viewPager = (ViewPager)getActivity().findViewById(R.id.vp_eight_imgs);
+
+    private void setView() {
+        tx_eight_title = (TextView) getActivity().findViewById(R.id.tx_eight_title);
+        tv_operate = (TextView) getActivity().findViewById(R.id.tv_operate);
+
+        viewPager = (ViewPager) getActivity().findViewById(R.id.vp_eight_imgs);
         LayoutInflater inflater = getActivity().getLayoutInflater();
-        img1 = inflater.inflate(R.layout.eight_img1,null);
-        img2 = inflater.inflate(R.layout.eight_img2,null);
+        img1 = inflater.inflate(R.layout.eight_img1, null);
+        img2 = inflater.inflate(R.layout.eight_img2, null);
+        img3 = inflater.inflate(R.layout.eight_img3, null);
+        img4 = inflater.inflate(R.layout.eight_img4, null);
         final ArrayList<View> list = new ArrayList();
         list.add(img1);
         list.add(img2);
+        list.add(img3);
+        list.add(img4);
 
         PagerAdapter pagerAdapter = new PagerAdapter() {
             @Override
             public int getCount() {
-                return  list.size();
+                return list.size();
             }
 
             @Override
@@ -63,9 +74,42 @@ public class FragmentEight extends Fragment {
             @Override
             public Object instantiateItem(ViewGroup container, int position) {
                 container.addView(list.get(position));
-                return  list.get(position);
+                return list.get(position);
             }
         };
         viewPager.setAdapter(pagerAdapter);
+        viewPager.setOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        tx_eight_title.setText("第一节  揉天应穴");
+                        tv_operate.setText("        以左右大拇指罗纹面接左右眉头下面的上眶角处。其他四指散开弯曲如弓状，支在前额上，按探面不要大。");
+                        break;
+                    case 1:
+                        tx_eight_title.setText("第二节  挤按晴明穴");
+                        tv_operate.setText("        以左手或右手大拇指按鼻根部，先向下按、然后向上挤。");
+                        break;
+                    case 2:
+                        tx_eight_title.setText("第三节  按揉四白穴");
+                        tv_operate.setText("        先以左右食指与中指并拢，放在靠近鼻翼两侧，大拇指支撑在下腭骨凹陷处，然后放下中指，在面颊中央按揉。注意穴位不需移动，按揉面不要太大。");
+                        break;
+                    case 3:
+                        tx_eight_title.setText("第四节  按太阳穴、轮刮眼眶");
+                        tv_operate.setText("        拳起四指，以左右大拇指罗纹面按住太阳穴，以左右食指第二节内侧面轮刮眼眶上下一圈，上侧从眉头开始，到眉梢为止，下面从内眼角起至外眼角止，先上后下，轮刮上下一圈。");
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 }
