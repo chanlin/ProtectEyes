3f2109336e733699de14b45ef0686849595407db
xlwei920@163.com <xlwei920@163.com>
2017-11-29 18:01:58 +0800

    add auto brightness

--- app/src/main/java/com/huayinghealth/protecteyes/utils/SystemShare.java
+++ app/src/main/java/com/huayinghealth/protecteyes/utils/SystemShare.java
@@ -25,7 +25,7 @@ public class SystemShare {
     public static void setSettingString(Context context, String key,
                                         String value) {
         SharedPreferences clientPreferences = context.getSharedPreferences(
-                PREFIX_NAME, 0);
+                PREFIX_NAME, Context.MODE_MULTI_PROCESS);
         SharedPreferences.Editor prefEditor = clientPreferences.edit();
         prefEditor.putString(key, value);
         prefEditor.commit();
@@ -37,7 +37,7 @@ public class SystemShare {
      */
     public static String getSettingString(Context context, String strKey) {
         SharedPreferences clientPreferences = context.getSharedPreferences(
-                PREFIX_NAME, 0);
+                PREFIX_NAME, Context.MODE_MULTI_PROCESS);
         String strValue = clientPreferences.getString(strKey, "");
         return strValue;
     }
@@ -49,7 +49,7 @@ public class SystemShare {
      */
     public static void ClearSettingString(Context context, String strKey) {
         SharedPreferences clientPreferences = context.getSharedPreferences(
-                PREFIX_NAME, 0);
+                PREFIX_NAME, Context.MODE_MULTI_PROCESS);
         SharedPreferences.Editor prefEditor = clientPreferences.edit();
         prefEditor.remove(strKey);
         prefEditor.commit();
@@ -63,7 +63,7 @@ public class SystemShare {
     public static void setSettingBoolean(Context context, String strKey,
                                          boolean value) {
         SharedPreferences clientPreferences = context.getSharedPreferences(
-                PREFIX_NAME, 0);
+                PREFIX_NAME, Context.MODE_MULTI_PROCESS);
         SharedPreferences.Editor prefEditor = clientPreferences.edit();
         prefEditor.putBoolean(strKey, value);
         prefEditor.commit();
@@ -76,7 +76,7 @@ public class SystemShare {
      */
     public static boolean getSettingBoolean(Context context, String strKey) {
         SharedPreferences clientPreferences = context.getSharedPreferences(
-                PREFIX_NAME, 0);
+                PREFIX_NAME, Context.MODE_MULTI_PROCESS);
         boolean value = clientPreferences.getBoolean(strKey, false);
         return value;
     }
@@ -84,7 +84,7 @@ public class SystemShare {
     public static boolean getSettingBoolean(Context context, String strKey,
                                             boolean value) {
         SharedPreferences clientPreferences = context.getSharedPreferences(
-                PREFIX_NAME, 0);
+                PREFIX_NAME, Context.MODE_MULTI_PROCESS);
         boolean ret = clientPreferences.getBoolean(strKey, value);
         return ret;
     }
@@ -97,7 +97,7 @@ public class SystemShare {
      */
     public static void setSettingInt(Context context, String strKey, int value) {
         SharedPreferences clientPreferences = context.getSharedPreferences(
-                PREFIX_NAME, 0);
+                PREFIX_NAME, Context.MODE_MULTI_PROCESS);
         SharedPreferences.Editor prefEditor = clientPreferences.edit();
         prefEditor.putInt(strKey, value);
         prefEditor.commit();
@@ -110,7 +110,7 @@ public class SystemShare {
      */
     public static int getSettingInt(Context context, String strKey) {
         SharedPreferences clientPreferences = context.getSharedPreferences(
-                PREFIX_NAME, 1);
+                PREFIX_NAME, Context.MODE_MULTI_PROCESS);
         int value = clientPreferences.getInt(strKey, 0);
         return value;
     }
